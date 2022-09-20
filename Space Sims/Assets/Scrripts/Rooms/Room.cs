ac47714c41fbfad171ad03ec0dc52ba5c2caa233
mwatt0204 <mwatt0204@gmail.com>
2022-09-20 16:55:06 +0100

    AddedBuildMenu
    had to rework code feels very messy will take some time to tidy it later on

--- Space Sims/Assets/Scrripts/Rooms/Room.cs
+++ Space Sims/Assets/Scrripts/Rooms/Room.cs
@@ -31,6 +31,7 @@ public class Room : MonoBehaviour, IInteractables
 
     private int level { get; set; } = 0; // overide setmethod at somepoint
     
+    [SerializeField]
     private RoomStats[] Roomlevels;
 
     public Vector3Int RoomPosition;
@@ -60,26 +61,51 @@ public class Room : MonoBehaviour, IInteractables
 
     public SkillsList DesiredSkill { get { return _desiredSkill; } }
 
-    [SerializeField]
-    private RoomStats Level1RoomStat;
-    [SerializeField]
-    private RoomStats Level2RoomStat;
-    [SerializeField]
-    private RoomStats Level3RoomStat;
+  //  [SerializeField]
+  //  private RoomStats Level1RoomStat;
+  //  [SerializeField]
+  //  private RoomStats Level2RoomStat;
+ //   [SerializeField]
+ //   private RoomStats Level3RoomStat;
     //enum skin :TODO   
+
+    private ResourcesEnum? _outputType = null;
+    public  ResourcesEnum? OutPutType { get { return _outputType; } }
+    public int? OutputValue { get {
+            if (OutPutType == null) return null;
+            return RoomStat.OutPut.GetResorce((ResourcesEnum)OutPutType);
+                } }
+
+    
+    private ResourcesEnum? _upkeepType = null;
+    public  ResourcesEnum? UpkeepType { get { return _upkeepType; } }
+    public int? UpkeepValue
+    {
+        get
+        {
+            if (UpkeepType == null) return null;
+            return -RoomStat.Upkeep.GetResorce((ResourcesEnum)UpkeepType);
+        }
+    }
+
+
+
 
 
     [SerializeField]
     private Camera camera;
-
-
     public RoomStats RoomStat { get { return Roomlevels[level]; } }
 
     void Start()
     {
-        Roomlevels = new RoomStats[3] {Level1RoomStat, Level2RoomStat, Level3RoomStat };
+        IntisaliseRoom();
         GlobalStats.Instance.PlyaerRooms.Add(this);
         GlobalStats.Instance.AddorUpdateRoomDelta(this, RoomStat.OutPut - RoomStat.Upkeep);
+    }
+
+    public void IntisaliseRoom()
+    {
+        SetUpkeepAndOutPut();
     }
 
     public bool Upgrade()
@@ -136,6 +162,27 @@ public class Room : MonoBehaviour, IInteractables
         camera.gameObject.SetActive(true);
         camera.targetTexture = renderTexture;
     }
+
+    private void SetUpkeepAndOutPut()
+    {
+        bool upkeepFound = false;
+        bool outPutFound = false;
+        foreach (ResourcesEnum re in Enum.GetValues(typeof(ResourcesEnum)))
+        {
+            if (RoomStat.Upkeep.GetResorce(re) != 0)
+            {
+                if (upkeepFound == true) { throw new Exception("Room " + gameObject.name + " has two diffrent Resources used for Upkeep"); }
+                upkeepFound = true;
+                _upkeepType = re;
+            }
+            if (RoomStat.OutPut.GetResorce(re) != 0)
+            {
+                if (outPutFound == true) { Debug.LogWarning("Room " +  gameObject.name + " has two diffrent Resources used for OutPut"); }
+                outPutFound = true;
+                _outputType = re;
+            }
+        }
+    }   
 
 
 #region InteractableInterface
