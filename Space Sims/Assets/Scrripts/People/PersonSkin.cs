7e87ed8c98147c6142502fcfe2c863e06bea33c6
Nathan Jenkins <nathan.jenkins1012@gmail.com>
2022-12-01 22:41:28 +0800

    People Parts Alpha Finalised

--- Space Sims/Assets/Scrripts/People/PersonSkin.cs
+++ Space Sims/Assets/Scrripts/People/PersonSkin.cs
@@ -24,40 +24,40 @@ static class PersonSkin
 
 };
 
-    private static Color[] AvianColors = new Color[5]
+    private static Vector3[] AvianColors = new Vector3[5]
     {
-        new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+     new Vector3(105, 222, 55),
+     new Vector3(55, 144, 222),
+     new Vector3(166, 55, 222),
+     new Vector3(222, 177, 55),
+     new Vector3(222, 55, 55),
     };
 
-    private static Color[] BovineColors = new Color[5]
+    private static Vector3[] BovineColors = new Vector3[5]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(134, 110, 44),
+     new Vector3(80, 77, 69),
+     new Vector3(174, 174, 174),
+     new Vector3(236, 236, 236),
+     new Vector3(182, 150, 141),
 };
 
-    private static Color[] CanineColors = new Color[5]
+    private static Vector3[] CanineColors = new Vector3[5]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(183, 110, 54),
+     new Vector3(230, 219, 120),
+     new Vector3(91, 91, 87),
+     new Vector3(236, 236, 236),
+     new Vector3(129, 158, 154),
 };
 
-    private static Color[] FelineColors = new Color[5]
+    private static Vector3[] FelineColors = new Vector3[5]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(253, 175, 86),
+     new Vector3(132, 130, 128),
+     new Vector3(90, 90, 90),
+     new Vector3(224, 235, 153),
+     new Vector3(206, 150, 86),
 };
 
     private static Vector3[] HumanColors = new Vector3[23]
@@ -86,40 +86,38 @@ static class PersonSkin
      new Vector3(125, 80, 59)
     };
 
-    private static Color[] InsectoidColors = new Color[5]
+    private static Vector3[] InsectoidColors = new Vector3[5]
     {
-        new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+     new Vector3(150, 253, 157),
+     new Vector3(150, 157, 253),
+     new Vector3(253, 216, 150),
+     new Vector3(253, 150, 150),
+     new Vector3(216, 150, 253),
     };
 
-    private static Color[] PlantColors = new Color[5]
+    private static Vector3[] PlantColors = new Vector3[5]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(117, 162, 104),
+     new Vector3(148, 162, 104),
+     new Vector3(162, 148, 104),
+     new Vector3(104, 162, 158),
+     new Vector3(162, 104, 133),
 };
 
-    private static Color[] ReptileColors = new Color[5]
+    private static Vector3[] ReptileColors = new Vector3[5]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(33, 236, 60),
+     new Vector3(110, 193, 37),
+     new Vector3(193, 163, 39),
+     new Vector3(195, 86, 39),
+     new Vector3(39, 195, 158),
 };
 
-    private static Color[] RoboticColors = new Color[5]
+    private static Vector3[] RoboticColors = new Vector3[3]
 {
-    new Color(255, 193, 144),
-     new Color(239, 159, 126),
-     new Color(244, 164, 129),
-     new Color(230, 166, 128),
-     new Color(233, 185, 149),
+    new Vector3(179, 179, 179),
+     new Vector3(124, 124, 124),
+     new Vector3(184, 115, 51)
 };
 
 
@@ -147,28 +145,28 @@ static class PersonSkin
             case Race.Avian:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = AvianColors[index];
+                    returnColor = new Color(AvianColors[index].x / 255, AvianColors[index].y / 255, AvianColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Bovine:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = BovineColors[index];
+                    returnColor = new Color(BovineColors[index].x / 255, BovineColors[index].y / 255, BovineColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Canine:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = CanineColors[index];
+                    returnColor = new Color(CanineColors[index].x / 255, CanineColors[index].y / 255, CanineColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Feline:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = FelineColors[index];
+                    returnColor = new Color(FelineColors[index].x / 255, FelineColors[index].y / 255, FelineColors[index].z / 255);
                     return returnColor;
                 }
 
@@ -176,35 +174,34 @@ static class PersonSkin
                 {
                     index = Random.Range(0, 22);
                     returnColor = new Color(HumanColors[index].x / 255, HumanColors[index].y / 255, HumanColors[index].z / 255);
-                    Debug.Log("I have chose index " + index + " which has a value of " + HumanColors[index]);
                     return returnColor;
                 }
 
             case Race.Insectoid:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = InsectoidColors[index];
+                    returnColor = new Color(InsectoidColors[index].x / 255, InsectoidColors[index].y / 255, InsectoidColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Plant:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = PlantColors[index];
+                    returnColor = new Color(PlantColors[index].x / 255, PlantColors[index].y / 255, PlantColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Reptile:
                 {
                     index = Random.Range(0, 4);
-                    returnColor = ReptileColors[index];
+                    returnColor = new Color(ReptileColors[index].x / 255, ReptileColors[index].y / 255, ReptileColors[index].z / 255);
                     return returnColor;
                 }
 
             case Race.Robotic:
                 {
-                    index = Random.Range(0, 4);
-                    returnColor = RoboticColors[index];
+                    index = Random.Range(0, 2);
+                    returnColor = new Color(RoboticColors[index].x / 255, RoboticColors[index].y / 255, RoboticColors[index].z / 255);
                     return returnColor;
                 }
         }
