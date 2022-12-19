6551f8322c6f75824c39ac3c34558d9919e25b37
mwatt0204 <mwatt0204@gmail.com>
2022-12-19 17:37:18 +0000

    chagnes

---
+++ Space Sims/Assets/PersonFootSteps.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonFootSteps : MonoBehaviour
+{
+    Vector3 LastPositon;
+
+    Person MainPersonScript;
+
+    private void Start()
+    {
+        MainPersonScript = gameObject.GetComponent<Person>();
+        LastPositon = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if(MainPersonScript.IsBeingHeld)
+        {
+            LastPositon = transform.position;
+            return;
+        }
+        else if(Mathf.Abs(Vector3.Distance(transform.position,LastPositon)) >= .5f)
+        {
+            LastPositon = transform.position;
+            SoundManager.Instance.PlaySoundIfVisableAndZoomedIn(SoundManager.Sound.MetalFootSteps, transform.position, 2.5f, 0.02f);
+        }
+    }
+}
